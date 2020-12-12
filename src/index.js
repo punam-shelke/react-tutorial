58e5d841c520135643970647a026e802d0ba7d99
punam-shelke <poonam.shelke@thoughtworks.com>
2020-12-12 20:05:11 +0530

    add variable and access them in JSX

--- src/index.js
+++ src/index.js
@@ -11,19 +11,18 @@ function BookList() {
     </section>
   );
 }
-
+const author = "Robert T. Kiyosaki";
 const Book = () => {
+  const title = `Rich Dad Poor Dad : What The Rich Teach Their Kids About Money That the
+        Poor and Middle Class Do Not!`;
   return (
     <article className="book">
       <img
         src="https://images-eu.ssl-images-amazon.com/images/I/91VokXkn8hL._AC_UL200_SR200,200_.jpg"
         alt="rich dad poor dad"
       />
-      <h1>
-        Rich Dad Poor Dad : What The Rich Teach Their Kids About Money That the
-        Poor and Middle Class Do Not!
-      </h1>
-      <h4>Robert T. Kiyosaki</h4>
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
     </article>
   );
 };
