7a18eac85aa59a61b8fd519b098117fb378044d8
punam-shelke <poonam.shelke@thoughtworks.com>
2020-12-12 19:55:07 +0530

    merge Image, Title, Author to Book component

--- src/index.js
+++ src/index.js
@@ -15,32 +15,17 @@ function BookList() {
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://images-eu.ssl-images-amazon.com/images/I/91VokXkn8hL._AC_UL200_SR200,200_.jpg"
+        alt="rich dad poor dad"
+      />
+      <h1>
+        Rich Dad Poor Dad : What The Rich Teach Their Kids About Money That the
+        Poor and Middle Class Do Not!
+      </h1>
+      <h4>Robert T. Kiyosaki</h4>
     </article>
   );
 };
-const Image = () => {
-  return (
-    <img
-      src="https://images-eu.ssl-images-amazon.com/images/I/91VokXkn8hL._AC_UL200_SR200,200_.jpg"
-      alt="rich dad poor dad"
-    />
-  );
-};
-
-const Title = () => (
-  <h1>
-    Rich Dad Poor Dad : What The Rich Teach Their Kids About Money That the Poor
-    and Middle Class Do Not!
-  </h1>
-);
-
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Robert T. Kiyosaki
-  </h4>
-);
 
 ReactDom.render(<BookList />, document.getElementById("root"));
