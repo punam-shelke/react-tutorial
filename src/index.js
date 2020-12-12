092ee0d66e1e2a27c1c362043d97011a43795c6b
punam-shelke <poonam.shelke@thoughtworks.com>
2020-12-12 20:12:36 +0530

    setup title and image as a variable at the start

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import ReactDom from "react-dom";
 import "./index.css";
 //setup vars
 const author = "Robert T. Kiyosaki";
-
+const title =
+  "Rich Dad Poor Dad : What The Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!";
+const image =
+  "https://images-eu.ssl-images-amazon.com/images/I/91VokXkn8hL._AC_UL200_SR200,200_.jpg";
 function BookList() {
   return (
     <section className="booklist">
@@ -16,14 +19,9 @@ function BookList() {
   );
 }
 const Book = () => {
-  const title = `Rich Dad Poor Dad : What The Rich Teach Their Kids About Money That the
-        Poor and Middle Class Do Not!`;
   return (
     <article className="book">
-      <img
-        src="https://images-eu.ssl-images-amazon.com/images/I/91VokXkn8hL._AC_UL200_SR200,200_.jpg"
-        alt="rich dad poor dad"
-      />
+      <img src={image} alt="rich dad poor dad" />
       <h1>{title}</h1>
       <h4>{author.toUpperCase()}</h4>
     </article>
